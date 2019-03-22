69b83a0653be00db47710dbfda7f1918295f8021
fibonacci1729 <brian@deis.com>
2019-03-22 09:02:36 -0600

    delete old code

--- pkg/packer/option.go
+++
@@ -1,7 +0,0 @@
-package packer
-
-func (opt option) Format() (verb rune) { return opt.verb }
-
-func (opt option) Width() (width uint) { return opt.width }
-
-func (opt option) Align() (align uint) { return opt.align }
